d0ed95a96d182a306d87d70a84fb079514783a28
BingBing <hwangwonbins@gmail.com>
2021-10-17 23:58:03 +0900

    egoing-react study 완강완료

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 
 import Toc from './components/Toc';
 import Subject from './components/Subject';
-import Content from './components/Content';
-
+import ReadContent from './components/ReadContent';
+import Control from './components/Control';
+import CreateContent from './components/CreateContent';
+import UpdateContent from './components/UpdateContent';
 class App extends Component {
   constructor(props) {
     super(props);
+    this.max_content_id = 3;
     this.state = {
       mode : "Welcome",
       selected_content_id: 2,
@@ -19,28 +22,67 @@ class App extends Component {
         {id: 2, title : "CSS", desc : "CSS is for information..."},
         {id: 3, title : "JavaScript", desc : "JavaScript is for information..."}
       ]
-
     }
   }
-  render() {
-    let _title, _desc = null;
+
+  getReadContent() {
+    for(let i = 0; i < this.state.contents.length; i++) {
+      let data = this.state.contents[i];
+      if(data.id === this.state.selected_content_id){
+        return data;
+        break;
+      }
+    } 
+  }
+
+  getContent() {
+    let _title, _desc, _article = null;
 
     if(this.state.mode === "Welcome") {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _article = <ReadContent 
+                  title = {_title} 
+                  desc = {_desc}>
+                </ReadContent>
     } else if(this.state.mode === "read") {
-      for(let i = 0; i < this.state.contents.length; i++) {
-        let data = this.state.contents[i];
-        if(data.id === this.state.selected_content_id){
-          _title = data.title;
-          _desc = data.desc;
-          break;
-        } 
+      let _content = this.getReadContent();
+      _article = <ReadContent title = {_content.title} desc = {_content.desc}></ReadContent>
+    } else if (this.state.mode === "create") {
+      _article = <CreateContent onSubmit= {(_title, _desc) => {
+        this.max_content_id = this.max_content_id + 1;
+        let _contents = Array.from(this.state.contents);
+        _contents.push({id:this.max_content_id, title: _title, desc: _desc})
+        this.setState({
+          contents: _contents,
+          mode: 'read',
+          selected_content_id: this.max_content_id,
+        })  
       }
+      }></CreateContent>
+    } else if (this.state.mode === "update") {
+      let _content = this.getReadContent();
+      _article = <UpdateContent data = {_content} onSubmit= {(_id ,_title, _desc) => {
+        let _contents = Array.from(this.state.contents);
+        for(let i = 0; i < _contents.length; i++) {
+          if(_contents[i].id === _id) {
+            _contents[i] = {id: _id, title: _title, desc: _desc}
+            break;
+          }
+        }
+        this.setState({
+          contents: _contents,
+          mode: 'read',
+        })  
+      }
+      }></UpdateContent>
     }
+    return _article;
+  }
+
+  render() {
     return (
       <div className="App">
-
         <Subject 
           title = {this.state.subject.title} 
           sub = {this.state.subject.sub}
@@ -61,16 +103,37 @@ class App extends Component {
         }} 
         data = {this.state.contents}>
         </Toc>
-
-        <Content 
-          title = {_title} 
-          desc = {_desc}>
-        </Content>
         
+        <Control onChangeMode = {
+          (_mode) => {
+            if(_mode === 'delete') {
+              if(window.confirm('really')){
+                let _contents = Array.from(this.state.contents)
+                for(let i = 0; i < _contents.length; i++) {
+                  if(_contents[i].id === this.state.selected_content_id) {
+                    _contents.splice(i, 1);
+                    break;
+                  }
+                  this.setState({
+                    mode: 'Welcome',
+                    contents: _contents,
+                  });
+                  alert('deleted');
+                }
+              }
+            } else {
+            this.setState({
+              mode : _mode,
+            });
+          }
+          }
+        }>
+        </Control>
+        {this.getContent()}
       </div>
     );
   }
 }
 
-//TODO: create 기능구현 목차 수강 필요 (10/17)
+//TODO: 2021.10/18일 수강 완료
 export default App;
